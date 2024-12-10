ced61c5a3952e9c528ecbedabbb6bd6d25b7009e
BrendanBezuko <brendanbbezuko@gmail.com>
2024-12-09 23:38:52 -0700

    first commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="myclass">
+    <p>Hello world</p>
+  </div>
+  <p class="otherClass">Hello world</p>
+</template>
+
+<style lang="scss">
+.otherClass {
+  color: $green;
+}
+</style>
