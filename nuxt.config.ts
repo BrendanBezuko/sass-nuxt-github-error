dd3549567a49ed809ca8023aafc0f42595a4e4d2
BrendanBezuko <brendanbbezuko@gmail.com>
2024-12-09 23:48:02 -0700

    changed ~ to @ syntax in scss nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           api: "modern-compiler",
-          additionalData: `@use "~/assets/scss/_colors.scss" as *;`,
+          additionalData: `@use "@/assets/scss/_colors.scss" as *;`,
         },
       },
     },
